53d0db32fcdb8fb3a3b7709536aae5a064a13660
Jason Wells <69333403+Bigjay2708@users.noreply.github.com>
2024-07-29 20:18:27 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import getPosts, { getPostLength } from './postController.js'; 
+
+console.log(getPosts());
+
+console.log(`Posts Length: ${getPostLength()}`);
+
+
+
+// const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
+
+// console.log(`Random Number: ${generateRandomNumber()}`);
+
+// console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`);
