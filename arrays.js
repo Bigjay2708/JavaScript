ee09eb9e3370cd349159a9f06fdc8480dffbd27a
Jason Wells <69333403+Bigjay2708@users.noreply.github.com>
2024-09-06 18:29:34 -0400

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3]; 
+arr.push(4);  // [1, 2, 3, 4] Adds 4 to the end of the array
+console.log(arr);
+
+arr.pop();    // [1, 2, 3]  Removes 4 from the end of the array
+console.log(arr);
+
+
+const arr1 = [5, 6];
+const arr2 = [7, 8];
+const combinedArr = arr1.concat(arr2);  // [1, 2, 3, 4] Combines arr1 and arr2 into a new array called combinedArr 
+console.log(combinedArr);
